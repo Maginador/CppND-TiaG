3195c9eadeed2a994dfacfc3f133a822da90ef93
Maginador <ricardo.maginador@wildlifestudios.com>
2021-02-01 23:19:15 -0400

    Refactor some classes structure to support GameObject concept

--- src/GameObject.cpp
+++ src/GameObject.cpp
@@ -4,5 +4,57 @@
 //
 //  Created by Ricardo Marginador on 25/01/21.
 //
-
 #include "GameObject.hpp"
+using std::unique_ptr;
+
+GameObject::GameObject(const char *name, Vector2 initialPosition, const SDL_Texture *tex, bool hasCollider ){
+    
+    //GameObject
+    _name = name;
+    
+    //Transform
+    _transform->_position = initialPosition;
+    
+    //Renderable
+    if(tex != nullptr){
+        //TODO: Add Renderable
+    }
+    //Collider
+    if(hasCollider){
+        //TODO: Add Collider
+    }
+};
+
+GameObject::~GameObject(){
+    //TODO: Object Destruction
+};
+
+
+void GameObject::addCollider(){
+    //TODO: a way to add collider on the fly
+}
+
+void GameObject::addRenderable(){
+    //TODO: a way to add renderable on the fly
+}
+
+
+/*
+Vector2& Vector2::operator=(Vector2 &b){
+  
+    this = b;
+};
+
+
+Transform::Transform(int x, int y){
+    this();
+    _position(x,y);
+    
+}
+
+Transform::Transform(Vector2 pos){
+    this();
+    _position(pos);
+}
+
+*/
--- src/GameObject.hpp
+++ src/GameObject.hpp
@@ -9,5 +9,51 @@
 #define GameObject_hpp
 
 #include <stdio.h>
+#include "Physics.hpp"
+#include "Renderer.hpp"
+#include <memory>
+#include <SDL2_image/SDL_image.h>
+using std::unique_ptr;
+
+struct Vector2{
+
+    int _x;
+    int _y;
+    Vector2(){};
+    Vector2(int x, int y);
+    //Vector2& operator=(Vector2 &b);
+    
+};
+
+class Transform{
+    public :
+    Transform(){};
+    ~Transform(){};
+    Transform(int x, int y);
+    
+    Transform(Vector2 pos);
+    
+    Vector2 _position;
+};
+
+class GameObject{
+  
+    public :
+    GameObject(const char *name = "new GameObject", Vector2 initialPosition = Vector2 (0,0), const SDL_Texture *tex = nullptr, bool hasCollider = false );
+    ~GameObject();
+    std::string getName(){ return _name;}
+    Collider* getCollider(){ return _collider.get();}
+    Renderable* getRenderable(){ return _renderable.get();}
+    void addCollider(); 
+    void addRenderable();
+    private :
+    std::unique_ptr<Collider> _collider = nullptr;
+    std::unique_ptr<Renderable> _renderable = nullptr;
+    std::unique_ptr<Transform> _transform = nullptr;
+    const char *_name;
+};
+
+
+
 
 #endif /* GameObject_hpp */
