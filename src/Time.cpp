a543ebd155209ff2148c1926fe2ba61f871d099e
Maginador <ricardo.maginador@wildlifestudios.com>
2021-02-15 02:11:35 -0400

    make enemy spawn a thread

--- src/Time.cpp
+++ src/Time.cpp
@@ -6,3 +6,20 @@
 //
 
 #include "Time.hpp"
+
+
+//TODO: Could use a delegate to make a generic way to call instead of return, but not sure how tu use a delegate here
+bool Time::timedAction(std::chrono::milliseconds time ){
+    
+    auto timer = std::chrono::system_clock::now() + time;
+    
+    while(true){
+        std::this_thread::sleep_for(std::chrono::milliseconds(1));
+        
+        
+        if(timer < std::chrono::system_clock::now()){
+            return true;
+        }
+    }
+    return false;
+}
--- src/Time.hpp
+++ src/Time.hpp
@@ -9,5 +9,12 @@
 #define Time_hpp
 
 #include <stdio.h>
+#include <chrono>
+#include <thread>
 
+class Time{
+  
+public:
+    static bool timedAction(std::chrono::milliseconds time);
+};
 #endif /* Time_hpp */
