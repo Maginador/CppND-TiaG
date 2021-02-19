c5004af35f70ff662cd5a583c4792df05c36e68d
Maginador <ricardo.maginador@wildlifestudios.com>
2021-02-19 01:24:48 -0400

    fix include for linux

--- src/Assets.hpp
+++ src/Assets.hpp
@@ -11,8 +11,8 @@
 #include <iostream>
 #include <map>
 #include "SDL2/SDL.h" 
-#include "SDL2_image/SDL_image.h"
-#include "SDL2_ttf/SDL_ttf.h"
+#include "SDL2/SDL_image.h"
+#include "SDL2/SDL_ttf.h"
 #include "Renderer.hpp"
 
 using std::string;
